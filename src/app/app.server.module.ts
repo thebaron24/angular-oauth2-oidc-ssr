4dfd2888a6c65512c14de5bd07f6006717c1cf38
thebaron24 <thebaron24@gmail.com>
2020-02-03 23:34:27 -0500

    removed unused import

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,7 +3,6 @@ import { ServerModule, ServerTransferStateModule } from '@angular/platform-serve
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
-import { AppRoutingModule } from './app-routing.module';
 import { AuthenticationServerModule } from './authentication/authentication.server.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UniversalInterceptor } from './universal.interceptor';
