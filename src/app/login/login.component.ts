a41840fbb31542ef108f62f9b374d03a8e662a6e
thebaron24 <thebaron24@gmail.com>
2020-02-03 22:45:02 -0500

    moved login button to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {}
+  constructor(public auth: AuthenticationService) {}
 
   ngOnInit() {}
 
